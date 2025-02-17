89ef65ab974cd5f58076b7ca551341608c67d313
Mousavi <sim.mousavi@gmail.com>
2025-02-17 09:38:32 +0330

    refactor: merge rabbitmq controller to producer controller

--- src/modules/producer/producer.controller.ts
+++ src/modules/producer/producer.controller.ts
@@ -1,13 +1,34 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { ProducerService } from './producer.service';
+import { RabbitmqService } from '../rabbitmq/rabbitmq.service';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('producer')
 export class ProducerController {
-  constructor(private readonly producerService: ProducerService) {}
+  constructor(
+    private readonly producerService: ProducerService,
+    private readonly rabbitmqService: RabbitmqService,
+  ) {}
 
-  @Get('simulate-xray')
+  @Get('generate-random-xray')
   async triggerDataSend() {
     const result = await this.producerService.simulateXRayData();
     return result;
+  }
+
+  @Post('send-xray')
+  @ApiBody({
+    description: 'send xray data',
+  })
+  async sendXRayData(@Body() xrayData: Record<string, any>) {
+    await this.rabbitmqService.sendMessage(xrayData);
+    return { status: 'success', receivedData: xrayData };
   }
 }
